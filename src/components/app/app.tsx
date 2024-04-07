2d8626e029aba07767922791e9167cbc27c9c2a1
Olga <151590@mail.ru>
2024-04-07 23:28:51 +0300

    добавляет роутинг

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,11 +1,36 @@
-import Main from '../../pages/main';
-// import Favorites from '../../pages/favorites';
-// import Login from '../../pages/login';
-import Offer from '../../pages/offer';
+import { Route, BrowserRouter, Routes } from 'react-router-dom';
+import { HelmetProvider } from 'react-helmet-async';
+import { AppRoute, AuthorizationStatus } from '../../const';
+
+import Main from '../../pages/main/main';
+import Offer from '../../pages/offer/offer';
+import Login from '../../pages/login/login';
+import Favorites from '../../pages/favorites/favorites';
+import NotFound from '../../pages/not-found/not-found';
+import PrivateRoute from '../../components/private-route/private-route';
 
 function App(): JSX.Element {
   return (
-    <Main />
+    <HelmetProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path={AppRoute.Root} element={<Main />} />
+          <Route path={AppRoute.Offer} element={<Offer />} />
+          <Route path={AppRoute.Login} element={<Login />} />
+          <Route
+            path={AppRoute.Favorites}
+            element={
+              <PrivateRoute authorizationStatus={AuthorizationStatus.NoAuth}>
+                <Favorites />
+              </PrivateRoute>
+            }
+            // element={<Favorites />}
+          />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </HelmetProvider>
+    // <Main />
     // <Favorites />
     // <Login />
     // <Offer />
--- src/pages/favorites/favorites.tsx
+++ src/pages/favorites/favorites.tsx
@@ -1,12 +1,16 @@
 // import FavoritesEmpty from '../components/favorites-empty/favorites-empty';
-import FavoritesList from '../components/favorites-list/favorites-list';
-import Footer from '../components/footer/footer';
-import Header from '../components/header/header';
+import { Helmet } from 'react-helmet-async';
+import FavoritesList from '../../components/favorites-list/favorites-list';
+import Footer from '../../components/footer/footer';
+import Header from '../../components/header/header';
 
 
 function Favorites(): JSX.Element {
   return (
     <div className="page">
+      <Helmet>
+        <title>6 городов. Избранное</title>
+      </Helmet>
       <Header />
       <main className="page__main page__main--favorites">
         <div className="page__favorites-container container">
